f004ea03f9618b8331fda7c9dd7d968ed1875e4b
cash <cash@cashwu.com>
2023-05-21 21:15:50 +0800

    add bmi

--- AvaloniaAppBmi2/MainWindow.axaml.cs
+++ AvaloniaAppBmi2/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AvaloniaAppBmi2;
 
@@ -7,5 +9,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ResultClick(object? sender, RoutedEventArgs e)
+    {
+        double.TryParse(TxtHeight.Text, out var height);
+        double.TryParse(TxtWeight.Text, out var weight);
+
+        TxtResult.Text = (weight / Math.Pow(height / 100, 2)).ToString("N");
     }
 }
